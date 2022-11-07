54144511de4e0a5207891da1157a585a36754917
AlexisCuvillier <alexis.cuvillier@protonmail.com>
2022-11-07 16:01:21 +0100

    alexis2

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello Simplon 1'));
+app.get('/hello', (req, res) => res.send('Hello Alexis'));
 
 app.listen(3001, ()=> console.log("Application démarrée sur le port Alexis: 3001"));
